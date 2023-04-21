c64e946bfec53fce4ccda881a068aa28185da59d
IAMDOGG <124060531+IAMDOGG@users.noreply.github.com>
2023-04-21 22:09:57 +0800

    Add files via upload

---
+++ Egg/Egg/BackTuyok.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egg
+{
+    public class BackTuyok:Tuyok
+    {
+        public void Tuyok()
+        {
+            Console.WriteLine("Back Tuyok sa itlog");
+        }
+    }
+}
---
+++ Egg/Egg/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egg
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Egg egg = new Egg(EggType.NOTWELLDONE);
+            FrontTuyok front = new FrontTuyok();
+            BackTuyok back = new BackTuyok();
+
+            egg.DisplayInfo();
+            front.Tuyok();
+            back.Tuyok();
+
+        }
+    }
+}
